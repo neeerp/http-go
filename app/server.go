4a5b0b8c4089048050adcb86330c4796d2e1f1cd
David Goldstein <goldstein.g.david@gmail.com>
2024-03-25 19:10:34 -0400

    Write HTTP 404 when non-root path given

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -22,6 +23,26 @@ func main() {
 	buf := make([]byte, 1024)
 	_, err = c.Read(buf)
 	fmt.Println("Read incoming request:\n", string(buf))
+
+	lines := strings.Split(string(buf), "\r\n")
+	if len(lines) < 1 {
+		fmt.Println("Startline missing!")
+		os.Exit(1)
+	}
+
+	start_line := lines[0]
+	parts := strings.Split(start_line, " ")
+	if len(parts) < 3 {
+		fmt.Println("Startline malformed!")
+		os.Exit(1)
+	}
+
+	path := parts[1]
+	if path != "/" {
+		msg := "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+		c.Write([]byte(msg))
+		os.Exit(1)
+	}
 
 	msg := "HTTP/1.1 200 OK\r\n\r\n"
 	c.Write([]byte(msg))
