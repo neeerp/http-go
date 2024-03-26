f0ea51aeafe811e7de8e7cb4455f474309152520
David Goldstein <goldstein.g.david@gmail.com>
2024-03-25 21:28:57 -0400

    Add header parsing

--- app/server.go
+++ app/server.go
@@ -30,34 +30,63 @@ func main() {
 		os.Exit(1)
 	}
 
-	start_line := lines[0]
-	parts := strings.Split(start_line, " ")
+	startLine := lines[0]
+	parts := strings.Split(startLine, " ")
 	if len(parts) < 3 {
 		fmt.Println("Startline malformed!")
 		os.Exit(1)
 	}
 
+	headers := parseHeaders(lines[1:])
 	path := parts[1]
-	route(path, c)
+	route(c, path, headers)
 }
 
-func route(path string, c net.Conn) {
+func parseHeaders(lines []string) map[string]string {
+	headers := make(map[string]string)
+	for _, l := range lines {
+		parsed := strings.SplitN(l, ": ", 2)
+		headers[parsed[0]] = parsed[1]
+	}
+
+	return headers
+}
+
+func route(c net.Conn, path string, headers map[string]string) {
 	if path == "/" {
 		msg := "HTTP/1.1 200 OK\r\n\r\n"
 		c.Write([]byte(msg))
 		os.Exit(0)
 	}
 
-	path_parts := strings.SplitN(path, "/", 3)
-	if len(path_parts) > 1 && path_parts[1] == "echo" {
-		content := path_parts[2]
-		content_length := len(content)
-		msg := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", content_length, content)
-		fmt.Println(msg)
-		c.Write([]byte(msg))
-		os.Exit(0)
+	pathParts := strings.SplitN(path, "/", 3)
+	if len(pathParts) > 1 {
+		switch pathParts[1] {
+		case "echo":
+			handleEcho(c, pathParts)
+		case "user-agent":
+			handleUserAgent(c, headers)
+		}
 	}
 
+	handleNotFound(c)
+}
+
+func handleEcho(c net.Conn, pathParts []string) {
+	content := pathParts[2]
+	contentLength := len(content)
+	msg := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, content)
+	fmt.Println(msg)
+	c.Write([]byte(msg))
+	os.Exit(0)
+}
+
+func handleUserAgent(c net.Conn, headers map[string]string) {
+	fmt.Sprintf("Not implemented yet!")
+	os.Exit(1)
+}
+
+func handleNotFound(c net.Conn) {
 	msg := "HTTP/1.1 404 NOT FOUND\r\n\r\n"
 	c.Write([]byte(msg))
 	os.Exit(1)
